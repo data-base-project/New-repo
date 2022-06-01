85faa5295074af91e3d9dde963590ae39f398d12
Ahmed Radwan <70094970+Ahmed01126@users.noreply.github.com>
2022-06-01 12:37:38 +0200

    update radwan

--- patient home/appointment/Home page abdo/sign up/js/landing.js
+++
@@ -1,9 +0,0 @@
-let menu = document.querySelector(".header .menu");
-menu.addEventListener("click", (e) => {
-    menu.classList.toggle("open");
-})
-document.body.addEventListener("click", (e) => {
-    if(!e.target.classList.contains("menu")){
-        menu.classList.remove("open");
-    }
-})
